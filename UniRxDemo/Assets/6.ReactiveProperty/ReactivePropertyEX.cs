e97e06ccbfaf97be220b35aaf0c9bb59d0f679d2
tujunyu@rocky.games <tjy940525>
2020-06-19 20:53:23 +0800

    xxxx

---
+++ UniRxDemo/Assets/6.ReactiveProperty/ReactivePropertyEX.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ReactivePropertyEX : MonoBehaviour
+{
+    //传统改变值 通知值的方式
+    //public Action<int> onAgeChanged = null;
+    //private int mAge = 0;
+    //public int Age {
+    //    get { return mAge; }
+    //    set {
+    //        if (mAge != value) {
+    //            mAge = value;
+    //            if (onAgeChanged != null) {
+    //                onAgeChanged(value);
+    //            }
+    //        }
+    //    }
+    //}
+
+    //void Start()
+    //{
+    //    onAgeChanged += Age => {
+    //        Debug.Log("age changed!!");
+    //    };
+    //}
+
+
+
+
+    public ReactiveProperty<int> Age = new ReactiveProperty<int>(0);
+
+    void Start()
+    {
+        Age.Subscribe(age =>
+        {
+            Debug.Log("age changed!!");
+        });
+
+        Age.Value = 15;
+    }
+
+}
